c318598371c7cd4229841c9f2518908cb41e2e1b
JS <joshua.t.stanford@gmail.com>
2025-11-18 12:58:21 -0500

    Partner 2 test functions completed

--- test_calculator.py
+++ test_calculator.py
@@ -4,11 +4,16 @@ from calculator import *
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
     # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self):
+        self.assertEqual(add(3,-3),0)
+        self.assertEqual(add(5,9),14)
+        self.assertEqual(add(2, -7), -5)
 
     # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_subtract(self):
+        self.assertEqual(sub(5, 4),1)
+        self.assertEqual(sub(-7, -2), -5)
+        self.assertEqual(sub(0, 2), -2)
 
     ######## Partner 1
     # def test_multiply(self): # 3 assertions
@@ -23,15 +28,21 @@ class TestCalculator(unittest.TestCase):
     #     # call division function inside, example:
     #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
     #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            div(1,0)
+
 
     # def test_logarithm(self): # 3 assertions
-    #     fill in code
-
+    def test_logarithm(self):
+        self.assertEqual(log(100,10), 2)
+        self.assertEqual(log(16 ,2), 4)
+        self.assertEqual(log(25, 5), 2)
     # def test_log_invalid_base(self): # 1 assertion
     #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
+    def test_log_invalid_base(self):
+        with self.assertRaises(ValueError):
+            log(8, 0)
     
     ######## Partner 1
     # def test_log_invalid_argument(self): # 1 assertion
@@ -50,7 +61,7 @@ class TestCalculator(unittest.TestCase):
     #     # Test basic function
     #     fill in code
     ##########################
-
+#on edstream after I push, then tell to pull
 # Do not touch this
 if __name__ == "__main__":
     unittest.main()
