1c866dbe74d04932222dfab4a56bc03929abc2c5
JS <joshua.t.stanford@gmail.com>
2025-11-19 13:13:45 -0500

    changed names of log and sub

--- calculator.py
+++ calculator.py
@@ -24,7 +24,7 @@ def hypotenuse(a, b):
 def add(a, b): 
     return a + b
 
-def sub(a, b): 
+def subtract(a, b):
     return a - b
 
 def mul(a, b): 
@@ -36,7 +36,7 @@ def div(a, b):
 
     return b / a # raise ZeroDivisionError if a == 0
 
-def log(a, b): 
+def logarithm(a, b):
     if a == 0:
         raise ValueError
     
